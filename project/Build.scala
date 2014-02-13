74776c9071c72f34cef109a051a079a08eae5962
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-02-13 14:09:45 +0100

    publish 0.90.10-THIB

--- project/Build.scala
+++ project/Build.scala
@@ -8,29 +8,33 @@ object Build extends Build {
   val basicSettings = Seq(
     organization := "org.scalastic",
     name := "scalastic",
-    version := "0.90.10",
+    version := "0.90.10-THIB",
     description := "a scala driver for elasticsearch",
     homepage := Some(url("https://github.com/bsadeh/scalastic")),
     licenses := Seq("The Apache Software License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
   )
 
   val scalaSettings = Seq(
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     scalacOptions ++= Seq("-unchecked", "-feature", "-deprecation")
   )
 
   val publishSettings = Seq(
-    publishMavenStyle := true,
-    publishArtifact in Compile := true,
-    publishArtifact in Test := false,
+    // publishMavenStyle := true,
+    // publishArtifact in Compile := true,
+    // publishArtifact in Test := false,
 
-    publishTo <<= version { (v: String) =>
-      val nexus = "https://oss.sonatype.org/"
-      if (v.trim.endsWith("SNAPSHOT"))
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases" at nexus + "service/local/staging/deploy/maven2")
-    },
+    // publishTo <<= version { (v: String) =>
+    //   val nexus = "https://oss.sonatype.org/"
+    //   if (v.trim.endsWith("SNAPSHOT"))
+    //     Some("snapshots" at nexus + "content/repositories/snapshots")
+    //   else
+    //     Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    // },
+    publishTo := Some(Resolver.sftp(
+      "iliaz",
+      "scala.iliaz.com"
+    ) as ("scala_iliaz_com", Path.userHome / ".ssh" / "id_rsa")),
 
     pomIncludeRepository := { _ => false },
 
@@ -66,13 +70,13 @@ object Build extends Build {
     resolvers += Resolver.sonatypeRepo("releases"),
 
     libraryDependencies ++= Seq(
-      "org.elasticsearch" % "elasticsearch" % "0.90.10" withSources,
+      "org.elasticsearch" % "elasticsearch" % "0.90.10",
       "com.google.code.findbugs" % "jsr305" % "1.3.9",
-      "org.clapper" %% "grizzled-slf4j" % "1.0.1" withSources,
+      "org.clapper" %% "grizzled-slf4j" % "1.0.1",
 
-      "junit" % "junit" % "4.10" % "test" withSources,
-      "org.scalatest" %% "scalatest" % "2.0" % "test" withSources,
-      "ch.qos.logback" % "logback-classic" % "1.0.2" % "test" withSources
+      "junit" % "junit" % "4.10" % "test",
+      "org.scalatest" %% "scalatest" % "2.0" % "test",
+      "ch.qos.logback" % "logback-classic" % "1.0.2" % "test"
     ),
 
     parallelExecution in Test := false
--- project/plugins.sbt
+++
@@ -1,8 +0,0 @@
-
-resolvers += "bt-idea-repo" at "http://mpeltonen.github.com/maven/"
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")
-
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
-
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
